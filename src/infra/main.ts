b868718180884289f3a952987bdf5c8c748ea5b9
BalvesD3v <lpbalves7filho@gmail.com>
2025-10-25 09:46:34 -0300

    feat: add nodemon and finish config in main.ts

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import express from "express";
+import { env } from "./config/env";
+
+const app = express();
+const port = env.PORT;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
