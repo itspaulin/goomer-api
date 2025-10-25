f37287aa12d25def8f901c97f06a938fa34d13e0
BalvesD3v <lpbalves7filho@gmail.com>
2025-10-25 15:52:35 -0300

    feat: add delete product use case and controller

---
+++ src/infra/http/controllers/delete-product.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { NotFoundError } from "@/domain/application/use-cases/errors/not-found.error";
+import { DeleteProductUseCase } from "@/domain/application/use-cases/delete-product.use-case";
+
+export class ProductsController {
+  constructor(private deleteProductUseCase: DeleteProductUseCase) {}
+
+  async delete(req: Request<{ id: string }>, res: Response): Promise<Response> {
+    const { id } = req.params;
+
+    const result = await this.deleteProductUseCase.execute({ id });
+
+    if (result.isLeft()) {
+      const error = result.value as NotFoundError;
+      return res.status(404).json({ message: error.message });
+    }
+
+    const { message } = result.value;
+    return res.status(200).json({ message });
+  }
+}
