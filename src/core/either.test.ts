98369ed76b24a150acefd441514071cbc19d7e0e
Paulo Barbosa <106263458+itspaulin@users.noreply.github.com>
2025-10-25 10:08:36 -0300

    Feat/create entities and services (#6)
    * feat: add entity.ts and unique-entity-id.ts to handle core context
    
    * feat: add optional types and use case errors
    
    * feat: add either to handle errors in use cases and add vitest to tests

---
+++ src/core/either.test.ts
@@ -0,0 +1,26 @@
+import { left, right, Either } from "./either";
+
+function doSomething(x: boolean): Either<string, number> {
+  if (x) {
+    return right(10);
+  } else {
+    return left("error");
+  }
+}
+
+test("sucess result", () => {
+  const result = doSomething(true);
+
+  if (result.isRight()) {
+    console.log(result.value);
+  }
+
+  expect(result.isRight()).toBe(true);
+  expect(result.isLeft()).toBe(false);
+});
+test("error result", () => {
+  const result = doSomething(false);
+
+  expect(result.isLeft()).toBe(true);
+  expect(result.isRight()).toBe(false);
+});
