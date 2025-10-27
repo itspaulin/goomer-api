0cd0b24a0ed537525ec9f94f98992896f8d2de0a
BalvesD3v <lpbalves7filho@gmail.com>
2025-10-27 18:35:36 -0300

    fix: add productRepository in create and update promotions factories

--- src/infra/factories/promotion/update/make-update-promotion-use-case.ts
+++ src/infra/factories/promotion/update/make-update-promotion-use-case.ts
@@ -1,7 +1,9 @@
 import { UpdatePromotionUseCase } from "@/domain/application/use-cases/update-promotion.use-case";
 import { makePromotionRepository } from "../make-promotion-repository";
+import { makeProductRepository } from "../make-product-repository";
 
 export function makeUpdatePromotionUseCase() {
   const promotionRepository = makePromotionRepository();
-  return new UpdatePromotionUseCase(promotionRepository);
+  const productRepository = makeProductRepository();
+  return new UpdatePromotionUseCase(promotionRepository, productRepository);
 }
